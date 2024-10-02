4182c905f3c53852e85ff95a6d3cf03675deb0e2
= <amirrezasokhankhosh79@gmail.com>
2024-10-02 16:16:27 -0500

    updating main branch

--- test-network/start.sh
+++
@@ -1,6 +0,0 @@
-./network.sh down
-./network.sh up
-./network.sh createChannel -c main
-./network.sh deployCC -ccp ../manage-scores/manage-scores-chaincode -ccn scoresCC -c main -ccl javascript
-./network.sh deployCC -ccp ../model-propose/model-propose-chaincode -ccn modelsCC -c main -ccl javascript
-./network.sh deployCC -ccp ../eval-propose/eval-propose-chaincode -ccn evalCC -c main -ccl javascript
