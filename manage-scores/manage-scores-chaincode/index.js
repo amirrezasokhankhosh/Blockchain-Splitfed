29f9d6372d62cd2bda574322141ab075ec509124
= <amirrezasokhankhosh79@gmail.com>
2024-10-02 17:24:28 -0500

    updating files

--- manage-scores/manage-scores-chaincode/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const manageScores = require('./lib/manageScores');
-
-module.exports.ManageScores = manageScores;
-module.exports.contracts = [manageScores];
--- test-network/req.sh
+++
@@ -1,3 +0,0 @@
-curl -X POST http://localhost:3000/api/scores/ledger
-curl -X POST http://localhost:3000/api/models/ledger
-curl -X POST http://localhost:3000/api/evals/ledger
