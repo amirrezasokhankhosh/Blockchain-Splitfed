8f22db0a2fcd2f2ab224a9042d0b499e9bc9b24c
amirrezasokhankhosh <amirrezasokhankhosh79@gmail.com>
2024-10-04 09:34:26 -0500

    attack completed with 36 nodes

--- run.py
+++ run.py
@@ -3,6 +3,110 @@ import time
 import subprocess
 import requests
 
+
+def create_node(i, num_clients=1, malicious=False):
+    code = f"""from global_var import *
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from split_fed.client import Client
+from split_fed.server import Server
+
+
+port = {8000 + i}
+num_clients = {num_clients}
+futures = {{}}
+executer = concurrent.futures.ThreadPoolExecutor(num_clients+1)
+client = Client(port, ClientNN, malicious={malicious})
+server = Server(port, ServerNN, client)
+app = Flask(__name__)
+
+
+@app.route("/client/train/")
+def train_client():
+    server_port = request.get_json()["server_port"]
+    executer.submit(client.train, server_port)
+    return "The client started training!"
+
+
+@app.route("/client/load/")
+def load_client():
+    client.load_model()
+    return "The client model is loaded."
+
+
+@app.route("/server/train/", methods=['POST'])
+def train_server():
+    client_port = request.get_json()["client_port"]
+    batch = request.get_json()["batch"]
+    clientOutputCPU = json.loads(request.get_json()["clientOutput"])
+    targets = torch.tensor(json.loads(request.get_json()["targets"]))
+
+    future = executer.submit(server.train, client_port, batch, clientOutputCPU, targets)
+    futures[client_port] = future
+
+    return {{
+        "status" : "In progress"
+    }}
+
+
+@app.route("/server/tasks/", methods=['POST'])
+def check_tasks():
+    client_port = request.get_json()["client_port"]
+    future = futures[client_port]
+    if not future.done():
+        return {{
+            "status" : "In progress"
+        }}   
+
+    grads, loss = future.result()
+    del futures[client_port]
+
+    return {{
+        "status" : "Completed",
+        "grads" : json.dumps(grads.tolist()),
+        "loss" : loss
+    }}
+
+
+@app.route("/server/round/", methods=['POST'])
+def round_completed():
+    client_port = request.get_json()["client_port"]
+    losses = request.get_json()["losses"]
+    server.finish_round(client_port, losses)
+    return "Well Done."
+
+@app.route("/server/models/ready/")
+def models_ready():
+    executer.submit(server.evaluate, client)
+    return "done"
+
+@app.route("/server/", methods=['POST'])
+def start_server():
+    temp = request.get_json()["clients"]
+    clients = [c["port"] for c in temp]
+    cycle = request.get_json()["cycle"]
+    executer.submit(server.start, clients, cycle)
+    return "Started."
+
+@app.route("/save/")
+def plot():
+    server.save_losses()
+    return "Done"
+
+@app.route("/exit/")
+def exit_miner():
+    os.kill(os.getpid(), signal.SIGTERM)
+
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=port, debug=True)
+"""
+    file = open(f"./node{i}.py", "w")
+    file.write(code)
+    file.close()
+
+
 def run_node(i):
     # Run node{i}.py in the background and redirect output to a log file
     log_file = f"../logs/node_{i}.txt"
@@ -16,8 +120,17 @@ def run_node(i):
         )
 
 if __name__ == "__main__":
-    num_nodes = 9
+    num_nodes = 36
+    num_malcious = 17
+    malicious_nodes = [False for _ in range(num_nodes)]
+    malicious_nodes[1:num_malcious+1] = [True for _ in range(num_malcious)]
     cwd = os.path.dirname(__file__)
+
+    os.chdir(os.path.join(cwd, "nodes"))
+    print("Create Nodes.")
+    create_node(0, num_clients=num_nodes-1)
+    for i in range(num_nodes-1):
+        create_node(i+1, malicious=malicious_nodes[i])
 
     # Step 1: Bring up miners (start processes)
     print("Bringing up the nodes...")
--- split_fed/client.py
+++ split_fed/client.py
@@ -1,6 +1,7 @@
 import time
 import json
 import torch
+import random
 import requests
 from torch import nn
 from pathlib import Path
@@ -39,15 +40,22 @@ class Client:
             download=False,
             transform=ToTensor()
         )
-        data_portion = len(training_dataset) // self.num_nodes
-        start_index = (self.port - 8000) * data_portion
-        end_index = (self.port - 8000 + 1) * data_portion
-        indexes = list(range(start_index, end_index))
 
-        test_portion = len(test_dataset) // self.num_nodes
-        test_start_index = (self.port - 8000) * test_portion
-        test_end_index = (self.port - 8000 + 1) * test_portion
-        test_indexes = list(range(test_start_index, test_end_index))
+        data_portion = len(training_dataset) // 9
+        indexes = [random.randint(0, len(training_dataset) - 1) for _ in range(data_portion)]
+
+        test_portion = len(test_dataset) // 9
+        test_indexes = [random.randint(0, len(test_dataset) - 1) for _ in range(test_portion)]
+
+        # data_portion = len(training_dataset) // self.num_nodes
+        # start_index = (self.port - 8000) * data_portion
+        # end_index = (self.port - 8000 + 1) * data_portion
+        # indexes = list(range(start_index, end_index))
+
+        # test_portion = len(test_dataset) // self.num_nodes
+        # test_start_index = (self.port - 8000) * test_portion
+        # test_end_index = (self.port - 8000 + 1) * test_portion
+        # test_indexes = list(range(test_start_index, test_end_index))
 
         self.training_dataset = torch.utils.data.Subset(training_dataset, indexes)
         self.test_dataset = torch.utils.data.Subset(test_dataset, test_indexes)
@@ -89,7 +97,7 @@ class Client:
                                         })
                     status = json.loads(res.content.decode())["status"]
                     while status == "In progress":
-                        time.sleep(0.1)
+                        time.sleep(0.2)
                         res = requests.post(f"http://localhost:{server_port}/server/tasks/",
                                             json={
                                                 "client_port" : self.port
@@ -126,7 +134,7 @@ class Client:
                                     })
                 status = json.loads(res.content.decode())["status"]
                 while status == "In progress":
-                    time.sleep(0.1)
+                    time.sleep(0.2)
                     res = requests.post(f"http://localhost:{server_port}/server/tasks/",
                                         json={
                                             "client_port" : self.port
