5487d79bc8a68b57a56c248e00382bfadc146449
= <amirrezasokhankhosh79@gmail.com>
2024-10-24 14:16:25 -0500

    plotting completed.

--- nodes/model.py
+++
@@ -1,7 +0,0 @@
-from global_var import *    
-
-
-server = ServerNN()
-client = ClientNN()
-torch.save(server.state_dict(), "./models/global_server.pth")
-torch.save(client.state_dict(), "./models/global_client.pth")
